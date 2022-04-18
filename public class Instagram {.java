998fa911496eae00907564cdaf6c3aec5030f09c
rizkyfarizh <92681249+rizkyfarizh@users.noreply.github.com>
2022-04-18 15:38:56 +0700

    Add files via upload

---
+++ public class Instagram {.java
@@ -0,0 +1,47 @@
+public class Instagram {
+
+//Matrix adalah suatu array di Java dapat dibentuk dengan mendefinisikan array dua dimensi. Kemudian kita dapat mengisi setiap elemen secara manual dengan mengakses setiap elemennya
+Melakukan Akses terhadap Elemen Matriks.
+Kita dapat memanfaatkan nested loop for untuk mengakses suatu elemen pada koordinat tertentu
+Melakukan Penjumlahan Terhadap Dua Matriks
+Melakukan Perkalian Terhadap Dua Matriks
+
+
+
+    double likes = 20;
+    double coment = 4;
+ 
+    String komentar;
+ 
+    Instagram(String komentar) {
+        this.komentar = komentar;
+    }
+        void likes() {
+            System.out.println("Disukai 200 orang dan lainnya.");
+        }
+     
+        void coment() {
+            System.out.println("Good this post.");
+        }
+     
+        void postcoment() {
+            System.out.println("Silahkan berkomentar.");
+        }
+
+        int getLikes() {
+            return Likes;
+        }
+     
+        double getComent() {
+            return Coment;
+        }
+     
+        double getPostComent() {
+            return postcoment();
+        }
+     
+    }
+
+}
+
+}
